9560a80ec352d6643b7786b6caaa754f57fac2bd
nforsyth <nforsyth@u.rochester.edu>
2025-07-09 16:40:18 -0400

    Added Blood Gem Item

--- src/main/java/net/lordolympian/lordmod/LordMod.java
+++ src/main/java/net/lordolympian/lordmod/LordMod.java
@@ -1,6 +1,8 @@
 package net.lordolympian.lordmod;
 
 import com.mojang.logging.LogUtils;
+import net.lordolympian.lordmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -28,7 +30,8 @@ public class LordMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
-        
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         
@@ -47,7 +50,9 @@ public class LordMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BLOOD_GEM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/lordolympian/lordmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.lordolympian.lordmod.item;
+
+import net.lordolympian.lordmod.LordMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, LordMod.MODID);
+
+    public static final RegistryObject<Item> BLOOD_GEM = ITEMS.register("bloodgem",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
